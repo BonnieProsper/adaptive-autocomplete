36a876216be6281552bfeafbc71980bf882c72d8
BonnieProsper <153881878+BonnieProsper@users.noreply.github.com>
2025-12-18 13:42:38 +1300

    Update contracts.py

--- src/aac/ranking/contracts.py
+++ src/aac/ranking/contracts.py
@@ -1,8 +1,8 @@
-from typing import Protocol
+from typing import Protocol, runtime_checkable
 
 from aac.domain.history import History
 
-
+@runtime_checkable
 class LearnsFromHistory(Protocol):
     """
     Contract for rankers that adapt based on user feedback.
