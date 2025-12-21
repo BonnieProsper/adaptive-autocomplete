426bb14511aff393ad0b4a3754d0ef0cfa667097
BonnieProsper <153881878+BonnieProsper@users.noreply.github.com>
2025-12-22 09:47:54 +1300

    Update frequency.py

--- src/aac/predictors/frequency.py
+++ src/aac/predictors/frequency.py
@@ -31,6 +31,9 @@ class FrequencyPredictor(Predictor):
                         score=score,
                         explanation=RankingExplanation.base(
                             value=word,
+                            base_score=1.0,
+                            history_boost=0.0,
+                            final_score=1.0,
                             score=score,
                             source="frequency",
                         ),
