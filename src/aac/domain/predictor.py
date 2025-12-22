142da0e8c9859a0847a4dddde438d705569d933b
BonnieProsper <153881878+BonnieProsper@users.noreply.github.com>
2025-12-23 10:33:56 +1300

    Update predictor.py

--- src/aac/domain/predictor.py
+++ src/aac/domain/predictor.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
 from typing import Protocol
 
 from aac.domain.types import CompletionContext, ScoredSuggestion
@@ -11,7 +12,13 @@ class Predictor(Protocol):
     Implementations should return a list of 'ScoredSuggestion' for a
     given input text.
     """
-    name: str
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        """
+        Name of the predictor.
+        """
+        raise NotImplementedError
 
     def predict(self, ctx: CompletionContext | str) -> list[ScoredSuggestion]:
         ...
