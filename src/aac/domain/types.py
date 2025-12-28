e3f40d2d039f9ef25d49b43cee467dfd13c5d528
BonnieProsper <153881878+BonnieProsper@users.noreply.github.com>
2025-12-29 12:36:40 +1300

    Update types.py

--- src/aac/domain/types.py
+++ src/aac/domain/types.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from collections.abc import Sequence
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Protocol
 
 
